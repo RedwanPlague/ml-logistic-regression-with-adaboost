e85eae2eb4b05f5d21bc90e3d79f35fd86321b21
RedwanPlague <mdredwan.rh@gmail.com>
2021-12-10 23:57:19 +0600

    added preprocessor for 2nd dataset (adult dataset)

--- main.py
+++ main.py
@@ -5,8 +5,32 @@ import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.feature_selection import mutual_info_classif
+
+
+def process_features(data, features_to_ignore, numerical_features, binary_features, categorical_features):
+    for col in features_to_ignore:
+        data = data.drop(col, axis=1)
+
+    mean_imputer = SimpleImputer(strategy='mean')
+    for col in numerical_features:
+        if data[col].isnull().sum() > 0:
+            data[col] = mean_imputer.fit_transform(data[[col]]).ravel()
+
+    frequent_imputer = SimpleImputer(strategy='most_frequent')
+    for col in categorical_features:
+        if data[col].isnull().sum() > 0:
+            data[col] = frequent_imputer.fit_transform(data[[col]]).ravel()
+
+    for col in binary_features:
+        data[col].replace({'Yes': 1, 'No': 0}, inplace=True)
+
+    for col in categorical_features:
+        data = pd.concat([data, pd.get_dummies(data[col], prefix=col, prefix_sep='=')], axis=1)
+        data = data.drop(col, axis=1)
+
+    return data
 
 
 def separate_labels(data, label_col_name):
@@ -32,8 +56,7 @@ def telco_customer_churn_preprocessor(use_count=None):
     df = pd.read_csv('datasets/telco-customer-churn.csv', na_values=' ')
 
     features_to_ignore = ['customerID']
-    numerical_features_to_impute = ['TotalCharges']
-    categorical_features_to_impute = []
+    numerical_features = ['tenure', 'MonthlyCharges', 'TotalCharges']
     binary_features = ['SeniorCitizen', 'Partner', 'Dependents', 'PhoneService', 'PaperlessBilling', 'Churn']
     categorical_features = ['gender', 'Contract', 'PaymentMethod', 'InternetService', 'MultipleLines',
                             'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
@@ -43,22 +66,7 @@ def telco_customer_churn_preprocessor(use_count=None):
 
     df['TotalCharges'] = df['TotalCharges'].astype('float')
 
-    for col in features_to_ignore:
-        df = df.drop(col, axis=1)
-
-    mean_imputer = SimpleImputer(strategy='mean')
-    for col in numerical_features_to_impute:
-        df[col] = mean_imputer.fit_transform(df[[col]]).ravel()
-
-    frequent_imputer = SimpleImputer(strategy='most_frequent')
-    for col in categorical_features_to_impute:
-        df[col] = frequent_imputer.fit_transform(df[[col]]).ravel()
-
-    for col in binary_features:
-        df[col].replace({'Yes': 1, 'No': 0}, inplace=True)
-
-    for col in categorical_features:
-        df = pd.concat([df, pd.get_dummies(df[col], prefix=col, prefix_sep='=')], axis=1).drop(col, axis=1)
+    df = process_features(df, features_to_ignore, numerical_features, binary_features, categorical_features)
 
     x, y = separate_labels(df, 'Churn')
 
@@ -66,6 +74,44 @@ def telco_customer_churn_preprocessor(use_count=None):
     x[x.columns] = min_max_scaler.fit_transform(x[x.columns])
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+    x_train, x_test = select_on_info_gain(x_train, y_train, x_test, use_count)
+
+    return x_train.T, x_test.T, y_train.T, y_test.T
+
+
+def adult_data_preprocessor(use_count=None):
+    train_df = pd.read_csv('datasets/adult/adult.data', header=None, na_values='?', sep=', ', engine='python')
+    test_df = pd.read_csv('datasets/adult/adult.test', skiprows=1, header=None, na_values='?', sep=', ',
+                          engine='python')
+
+    features = ['age', 'work-class', 'final-weight', 'education', 'education-num', 'marital-status', 'occupation',
+                'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
+                '>50K']
+    features_to_ignore = []
+    numerical_features = ['age', 'final-weight', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+    binary_features = ['>50K']
+    categorical_features = ['work-class', 'education', 'marital-status', 'occupation',
+                            'relationship', 'race', 'sex', 'native-country']
+
+    train_df.columns = features
+    test_df.columns = features
+
+    train_df['>50K'].replace({'>50K': 'Yes', '<=50K': 'No'}, inplace=True)
+    test_df['>50K'].replace({'>50K.': 'Yes', '<=50K.': 'No'}, inplace=True)
+
+    split_index, _ = train_df.shape
+    df = train_df.merge(test_df, how='outer')
+
+    df = process_features(df, features_to_ignore, numerical_features, binary_features, categorical_features)
+
+    min_max_scaler = MinMaxScaler()
+    df[df.columns] = min_max_scaler.fit_transform(df[df.columns])
+
+    train_df, test_df = df.iloc[:split_index, :], df.iloc[split_index:, :]
+
+    x_train, y_train = separate_labels(train_df, '>50K')
+    x_test, y_test = separate_labels(test_df, '>50K')
 
     x_train, x_test = select_on_info_gain(x_train, y_train, x_test, use_count)
 
@@ -157,16 +203,26 @@ class AdaBoost:
 
 def main():
     x_train, x_test, y_train, y_test = telco_customer_churn_preprocessor()
-    ab = AdaBoost([LogisticRegressor(alpha=0.1, max_iterations=2000, error_cutoff=0.0) for _ in range(10)])
-    ab.fit(x_train, y_train)
-    train_pred = ab.predict(x_train)
-    test_pred = ab.predict(x_test)
-    # lr = LogisticRegressor(alpha=0.1, max_iterations=2000, error_cutoff=0.0)
-    # lr.fit(x_train, y_train)
-    # train_pred = lr.predict(x_train)
-    # test_pred = lr.predict(x_test)
+    # x_train, x_test, y_train, y_test = adult_data_preprocessor()
+
+    # ab = AdaBoost([LogisticRegressor(alpha=0.1, max_iterations=2000, error_cutoff=0.0) for _ in range(10)])
+    # ab.fit(x_train, y_train)
+    # train_pred = ab.predict(x_train)
+    # test_pred = ab.predict(x_test)
+
+    lr = LogisticRegressor(alpha=0.1, max_iterations=2000, error_cutoff=0.0)
+    lr.fit(x_train, y_train)
+    train_pred = lr.predict(x_train)
+    test_pred = lr.predict(x_test)
+
     print('Train Accuracy: {:.2f}'.format(accuracy(y_train, train_pred)))
     print('Test  Accuracy: {:.2f}'.format(accuracy(y_test, test_pred)))
+    print('Train Precision: {:.2f}'.format(precision_score(y_train.ravel(), train_pred.ravel())))
+    print('Test  Precision: {:.2f}'.format(precision_score(y_test.ravel(), test_pred.ravel())))
+    print('Train Recall: {:.2f}'.format(recall_score(y_train.ravel(), train_pred.ravel())))
+    print('Test  Recall: {:.2f}'.format(recall_score(y_test.ravel(), test_pred.ravel())))
+    print('Train F1 Score: {:.2f}'.format(f1_score(y_train.ravel(), train_pred.ravel())))
+    print('Test  F1 Score: {:.2f}'.format(f1_score(y_test.ravel(), test_pred.ravel())))
 
 
 if __name__ == '__main__':
